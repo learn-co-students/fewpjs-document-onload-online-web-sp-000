f7171e9eab703017eb47d0ad180c0f56d16eac38
Aliciea Dawn Suechin <aliciea@Alicieas-MacBook-Pro.local>
2019-10-03 23:07:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    // console.log("The DOM has loaded");
+    addText()
+
+});
+
+function addText() {
+    document.getElementById('text')
+        .innerHTML = 'This is really cool!';
+}
+
