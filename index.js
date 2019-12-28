59f52db2aa02785489cd3a60c9e5cf4a50e609b5
Karen V <karenvo@usc.edu>
2019-12-28 04:51:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+// `JavaScript is so cool. It lets me add text to my page programmatically.`
+
+document.addEventListener("DOMContentLoaded", function() {
+  text = document.getElementById("text")
+  text.innerHTML = "This is really cool!";
+});
