c167c5bf57168e3209d61c6a69a1654adf9b56f0
Soner Mezgitci <you@example.com>
2020-02-27 23:24:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//   });
+
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+// let itemInput = document.getElementById('p[id=text');
+// itemInput.textContent = "This is really cool!";
+// var itemInput = document.querySelector('p[id ="text"]').textContent = "This is really cool!";
+
+
+
+
+document.addEventListener("DOMContentLoaded", theDomHasLoaded, false);
+
+function theDomHasLoaded(e) {
+ let event = document.querySelector("p");
+    event.textContent = "This is really cool!"
+
+   }
