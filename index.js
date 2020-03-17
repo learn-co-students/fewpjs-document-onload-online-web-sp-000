6812e745f5716e3b62e2b70551f4ed8c000bbbbf
Ronald Stephens <rstephens@bluemorpho.cc>
2020-03-17 10:08:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-document.addEventListener("DOMContentLoaded",function() {
-    console.log("The DOM is loaded");
-        let element = document.querySelector("p#text");
-        element.innerHTML = "This is really cool!";
-    });
+document.addEventListener("DOMContentLoaded", function(){
+    updateDOM()
+});
 
-console.log(
-    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-);
+function updateDOM() {
+    document.getElementById("text")
+        .innerHTML = "This is really cool!";
+}
