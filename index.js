f19b4ea09935c15af3174ee840ba0b6b659259b3
github username <github email address>
2020-04-15 18:24:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+let paraf = document.getElementById("text");
+
+document.addEventListener("DOMContentLoaded", function() {
+  paraf.textContent = "This is really cool!";
+});
+
