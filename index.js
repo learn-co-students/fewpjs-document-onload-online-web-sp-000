a988c7c334e29a42b035d532b556d4707dc3f9eb
andrewsilva9 <andrewsilva9@gmail.com>
2020-05-18 12:25:18 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    refreshDocument()
+  });
+
+function refreshDocument() {
+    document.getElementById('text')
+    .innerHTML = "This is really cool!";
+}
