698eafa8565f75f6355de043261f2c0050973fe1
github username <github email address>
2020-06-06 22:17:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const text = document.body.querySelector("#text");
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    text.innerHTML = "This is really cool!";
+});
+
