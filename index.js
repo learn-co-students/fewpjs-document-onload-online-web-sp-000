b2af0157e95f48805c5eb9e9acced6c3565a0fe3
Anthonyntilelli <Anthony@tilelli.me>
2020-07-15 08:50:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", alterText());
+
+function alterText(){
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
