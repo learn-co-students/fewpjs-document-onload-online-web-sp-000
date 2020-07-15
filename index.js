a3e84b11cefdb7e570de044b5f313b2e8400b1f8
Anthonyntilelli <Anthony@tilelli.me>
2020-07-15 08:54:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-// Your code goes here
-document.addEventListener("DOMContentLoaded", alterText());
+// // Your code goes here
+document.addEventListener("DOMContentLoaded", alterText);
 
 function alterText(){
     document.getElementById("text").innerHTML = "This is really cool!";
 }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById("text").innerHTML = "This is really cool!";
+//   });
