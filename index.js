f567d6676f24e4f271a3caadfade14f4a42135bb
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-30 15:48:48 -0400

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
 document.addEventListener("DOMContentLoaded", function() {
   text.textContent = ("This is really cool!");
 });
+
+/* flatiron solution
+   
+document.addEventListener( "DOMContentLoaded", function () {
+  updateDOM()
+} );
+
+
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+}
+
+*/
