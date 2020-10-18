23c10ff6a1b1a6af950888e59a035f66ca8437bc
murphcoder <murphcoder@gmail.com>
2020-10-18 08:48:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+let text = document.getElementById("text");
+
+document.addEventListener("DOMContentLoaded", function() {
+    text.innerHTML = "This is really cool!";
+  });
