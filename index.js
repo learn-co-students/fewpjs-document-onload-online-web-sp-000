f835e90810b13a8689a281aa4b96a035d0b9311d
jeanettepayne <jeanetterpayne@gmail.com>
2021-03-11 21:14:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+const pageText = document.getElementById("text")
+
+document.addEventListener("DOMContentLoaded", function() {
+    pageText.innerHTML = "This is really cool!";
+  });
