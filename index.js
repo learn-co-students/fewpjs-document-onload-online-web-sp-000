eab2a2e7b77694a5aa64f00b361daef399709b18
Tres Tarwater <trest03@gmail.com>
2021-03-26 18:29:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let answer = document.getElementById('text');
+    answer.innerHTML = "This is really cool!"
+});
