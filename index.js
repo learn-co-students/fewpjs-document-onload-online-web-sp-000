df8bff423f5cb602640da22ddd81ff5be2e9a498
amcmullen9291 <amcmullen9291@gmail.com>
2021-03-28 02:23:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    updateIdText()
+  });
+
+  console.log("The DOM  outside of the function has loaded");
+
+  function updateIdText() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
