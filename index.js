245f9bab1300b9f715e7aa488759ee11dfaf478d
Lj <tnedutsl457892@gmail.com>
2021-06-30 21:59:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// Change the text on the page when DOM content has loaded
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector('p#text').innerHTML = "This is really cool!"
+});
+ 
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
